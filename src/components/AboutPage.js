84bf0b1b632d9a9fc22bf3a94b332a885a8345c2
Hernandez Carmona Samuel Karim <mb78200@MB78200MAC.local>
2020-04-24 20:54:40 -0500

    React Fragment added to display html components

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -4,7 +4,22 @@ class AboutPage extends React.Component {
   //Classes need just one required method: render
   render() {
     //We declare our JSX
-    return <h2>About</h2>;
+    /*
+    JSX is compiled down to function calls. You can
+    only have one top-level function.
+
+    React.Fragement allow us to wrap adjacent elements,
+    but React won't render anything.
+    React.Fragment == <> </>
+    It is recommended to use <>, as an easy way anytime
+    you have adjacent JSX elements
+    */
+    return (
+      <>
+        <h2>About</h2>
+        <p>This app uses React.</p>
+      </>
+    );
   }
 }
 
