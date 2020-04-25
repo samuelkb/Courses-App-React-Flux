618b95af11a2884061a1c06258ad3073c6bddee5
Hernandez Carmona Samuel Karim <mb78200@MB78200MAC.local>
2020-04-24 20:28:26 -0500

    index reference to HomePage added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { render } from "react-dom";
+import HomePage from "./components/HomePage";
+
+//create-react-app looks at index.js to determine what files are in your app.
+/* 
+Line 3 creates a const alled render that references react-dom's render
+function. Equivalent to:
+import ReactDOM from "react-dom";
+const render = ReactDOM.render; 
+*/
+
+/*
+Render recives two arguments.
+The first one is the component that we want to render.
+JSX supports self-closing tags much like HTML.
+The second argument, we need to tell React which DM element it should render into
+*/
+render(<HomePage />, document.getElementById("root"));
+//We use the div called 'root' defined on index.html, line 31.
