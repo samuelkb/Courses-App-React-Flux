e31060ebf26d85cc2fc1222f443d3cb697ba701f
Hernandez Carmona Samuel Karim <mb78200@MB78200MAC.local>
2020-04-24 20:42:03 -0500

    AboutPage created

---
+++ src/components/AboutPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  //Classes need just one required method: render
+  render() {
+    //We declare our JSX
+    return <h2>About</h2>;
+  }
+}
+
+export default AboutPage;
