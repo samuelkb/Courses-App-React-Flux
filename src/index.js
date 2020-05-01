b841c23326287100bda4d8cd8dc44195a2b7c3b0
Hernandez Carmona Samuel Karim <mb78200@MB78200MAC.local>
2020-04-30 22:20:22 -0500

    Router component added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
+import { BrowserRouter as Router } from "react-router-dom";
+//The 'as' word only makes easy the call to browser router, it's optional
 
 //create-react-app looks at index.js to determine what files are in your app.
 /* 
@@ -17,5 +19,10 @@ The first one is the component that we want to render.
 JSX supports self-closing tags much like HTML.
 The second argument, we need to tell React which DM element it should render into
 */
-render(<App />, document.getElementById("root"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 //We use the div called 'root' defined on index.html, line 31.
